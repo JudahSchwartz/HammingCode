26522ac86abd90e9a2693f078e17377755b4405a
Judah Schwartz <judahsch@gmail.com>
2018-12-09 14:27:53 -0500

    added wrap up

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
                 "\n" +
                 "But, in a larger sense, we can not dedicate -- we can not consecrate -- we can not hallow -- this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth.";
         message = message.toLowerCase();
+
         Map<Character,Long> map = getLetterMap(message);
         SortedList list = new SortedList(map);
         while(list.size()>=2)
@@ -29,6 +30,10 @@ public class Main {
 
 
         System.out.println("Decoded : " + decode(flipMap(dictionary),encodedMessage));
+        int size = 0;
+
+        System.out.println("Bits without encoding needed : " +( size = (int) Math.ceil(Math.log(map.size())/Math.log(2) * message.length() )));
+        System.out.println("Bits used with encoding : " + encodedMessage.length());
 
     }
 
